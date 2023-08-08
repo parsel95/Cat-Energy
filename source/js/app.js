f63f1f8cddb0746ec28939084dbe98d94cb90bc8
parsel95 <venger090239@gmail.com>
2023-08-08 16:41:48 +0200

    Скрипт на toggler

--- source/js/app.js
+++ source/js/app.js
@@ -12,3 +12,33 @@ buttonElement.addEventListener('click', () => {
     navElement.classList.remove('page-header__nav--opened');
   }
 });
+
+let exampleImgages = document.querySelector('.example__imgages');
+let switcherButtonBefore = document.querySelector('.switcher__button--before');
+let switcherButtonAfter = document.querySelector('.switcher__button--after');
+let switcherBar = document.querySelector('.switcher__bar');
+let switcherToggler = document.querySelector('.switcher__toggler');
+let exampleImgageBefore = document.querySelector('.example__image--before');
+let exampleImgageAfter = document.querySelector('.example__image--after');
+
+switcherButtonBefore.onclick = function () {
+  exampleImgages.style.gridTemplateColumns = '100% 1fr';
+  switcherBar.value = '0';
+  exampleImgageBefore.classList.add('example__image--active');
+  exampleImgageAfter.classList.remove('example__image--active');
+  switcherToggler.classList.add('switcher__toggler--before');
+  switcherToggler.classList.remove('switcher__toggler--after');
+}
+
+switcherButtonAfter.onclick = function () {
+  exampleImgages.style.gridTemplateColumns = '0% 1fr';
+  switcherBar.value = '100';
+  exampleImgageAfter.classList.add('example__image--active');
+  exampleImgageBefore.classList.remove('example__image--active');
+  switcherToggler.classList.add('switcher__toggler--after');
+  switcherToggler.classList.remove('switcher__toggler--before');
+}
+
+switcherBar.oninput = function () {
+  exampleImgages.style.gridTemplateColumns = '1fr ' + switcherBar.value + '%';
+}
